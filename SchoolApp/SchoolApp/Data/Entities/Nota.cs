85b2275986af2e078e145ea1d1f18f713f29b366
Catia0202 <catiaalexandrasilvasantos@gmail.com>
2022-09-30 14:09:18 +0100

    new class Nota

--- SchoolApp/SchoolApp/Data/Entities/Nota.cs
+++ SchoolApp/SchoolApp/Data/Entities/Nota.cs
@@ -1,14 +1,23 @@
-﻿namespace SchoolApp.Data.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolApp.Data.Entities
 {
     public class Nota
     {
         public int Id { get; set; }
-        public string NotaAluno { get; set; }
+        public int NotaAluno { get; set; }
 
         public string Anotação { get; set; }
 
-        public Aluno cod_aluno { get; set; }
-        public Disciplina cod_disciplina { get; set; }
+        public int id_aluno { get; set; }
+        public Aluno aluno { get; set; }
 
+        public int id_disciplina { get; set; }
+        public Disciplina disciplina { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+        public DateTime Data { get; set; }
     }
 }
