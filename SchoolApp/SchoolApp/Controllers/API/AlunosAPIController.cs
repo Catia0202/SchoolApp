daf064c60407d6cb3f2992f39acd7e4f17920199
Catia0202 <catiaalexandrasilvasantos@gmail.com>
2022-10-05 17:52:58 +0100

    API Alunos

---
+++ SchoolApp/SchoolApp/Controllers/API/AlunosAPIController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolApp.Data;
+
+namespace SchoolApp.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosAPIController : Controller
+    {
+        private readonly IAlunosRepository _alunosRepository;
+
+        public AlunosAPIController(IAlunosRepository alunosRepository)
+        {
+           _alunosRepository = alunosRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAlunos()
+        {
+            return Ok(_alunosRepository.GetAll());
+        }
+    }
+}
